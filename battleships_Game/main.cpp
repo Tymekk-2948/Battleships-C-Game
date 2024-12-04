73dffa671d4ec5c08b6c45c0c1c261832d13bbb2
Tymekk-2948 <paraptsyk@gmail.com>
2024-12-04 10:32:33 +0100

    main.cpp

--- battleships_Game/main.cpp
+++ battleships_Game/main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/*#include <iostream>
 #include "Player.h"
 #include <cstdlib>
 #include <limits>
@@ -35,9 +35,63 @@ int main() {
                     cout << "Player 2 wins!"<< endl;
                     gameOver = true;
                 }
+                board.confirmAndClear();
+                board.clearScreen();
             }
-            board.confirmAndClear();
-            board.clearScreen();
+        }
+    }
+
+    return 0;
+}
+*/
+
+#include <iostream>
+#include "Player.h"
+#include <cstdlib>
+#include <limits>
+using namespace std;
+
+int main() {
+    Player player1, player2;
+
+    cout << "Player 1, place your ships:" << endl;
+    player1.placeFleet();
+    player1.board.clearScreen();
+
+    cout << "Player 2, place your ships:" << endl;
+    player2.placeFleet();
+    player1.board.clearScreen();
+
+    bool gameOver = false;
+    while (!gameOver) {
+        // Tura gracza 1
+        cout << "Player 1's turn:" << endl;
+        if (player1.takeShot(player2)) {
+            if (player2.board.allShipsSunk()) {
+                cout << "Player 1 wins!" << endl;
+                gameOver = true;
+            }
+        }
+        if (!gameOver) {
+            player1.board.confirmAndClear();
+            player1.board.clearScreen();
+        }
+
+        if (gameOver) {
+            player1.board.confirmAndClear();
+            break;
+        }
+
+        cout << "Player 2's turn:" << endl;
+        if (player2.takeShot(player1)) {
+            if (player1.board.allShipsSunk()) {
+                cout << "Player 2 wins!" << endl;
+                gameOver = true;
+            }
+        }
+        if (!gameOver) {
+            player2.board.confirmAndClear();
+            player1.board.clearScreen();
         }
     }
 
